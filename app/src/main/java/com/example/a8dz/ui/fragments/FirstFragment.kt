7bce337a10e6c840f06d68a4b0046be831eede79
Nursultanakmatbekov <nursultanakmatbekov@gmail.com>
2023-01-28 17:02:17 +0600

    фывапп

--- app/src/main/java/com/example/a8dz/ui/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/a8dz/ui/fragments/FirstFragment.kt
@@ -1,22 +1,13 @@
 package com.example.a8dz.ui.fragments
 
-import android.app.AlarmManager
-import android.app.Notification
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
 import com.example.a8dz.databinding.FragmentFirstBinding
 import com.example.a8dz.ui.utils.MyNotificationService
-import com.example.a8dz.ui.utils.edtextExtra
-import com.example.a8dz.ui.utils.notificationID
-import java.util.Calendar
-import kotlin.concurrent.timer
+import com.example.a8dz.ui.utils.editTextExtra
 
 class FirstFragment : Fragment() {
 
@@ -42,9 +33,9 @@ class FirstFragment : Fragment() {
     }
 
     private fun setUpListeners() {
-        val edtext = binding?.edText?.text.toString()
         binding?.btnSend?.setOnClickListener {
-            edtextExtra = edtext
+            val edtext = binding?.edText?.text.toString().trim()
+            editTextExtra = edtext
             service.showNotification()
         }
     }
